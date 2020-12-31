148104d99a48cbe928b566b57173094d5aaec517
JuminaCahyani40 <60830909+JuminaCahyani40@users.noreply.github.com>
2020-12-31 11:57:44 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -54,8 +54,8 @@ if (!isset($_SESSION["is_logged"])) {
                         <li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Laporan <span class="caret"></span></a>
                           <ul class="dropdown-menu">
-                            <li><a href="?page=lap_seluruh">Seluruh Calon Mahasiswa Baru</a></li>
-                            <li><a href="?page=lap_percamaba">Per Calon Mahasiswa Baru</a></li>
+                            <!--<li><a href="?page=lap_seluruh">Seluruh Calon Mahasiswa Baru</a></li>-->
+                            <!--<li><a href="?page=lap_percamaba">Per Calon Mahasiswa Baru</a></li>-->
                             <li><a href="?page=lap_pendaftaran">Pendaftaran</a></li>
                           </ul>
                         </li>
